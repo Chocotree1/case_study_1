d33082e8e3d570fce2a88972e2778238eb24fd40
matthpatth <thealexzaytsev@gmail.com>
2019-10-12 17:11:08 +0700

    commit1

--- case_study_1.py
+++ case_study_1.py
@@ -3,10 +3,25 @@
 #            Sharypov R.
 #            Kurasova P.
 import turtle
-
 #TODO ALEXANDER
+turtle.penup()
+turtle.goto(-100,0)
+turtle.pencolor('yellow')
+turtle.begin_fill()
+turtle.color('blue')
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(50)
+turtle.end_fill()
+turtle.up()
+turtle.mainloop()
 
-
+turtle
 #TODO ROMAN
 #TODO POLINA
 print('VSEM hi')

09146d3b3573453019818a8aae3216fb15256674
Roman <sharypovr@mail.ru>
2019-10-12 16:37:23 +0700

    commit problem

--- project1.py
+++ project1.py
@@ -1,6 +1,6 @@
-WorldWater=35000000 #Воды пресной всего в км^3
-GrandLakesWater=WorldWater*1000000000 #Всего пресной воды в мире в м^3
-GrandLakesWatercorr=GrandLakesWater*0.21 #Всего пресной воды в Великих озерах в м^3
-Square=7664000 #Площадь 48 штатов США в км^2
-Squarecorr=Square*1000000 #Площадь 48 штатов США в м^2
-print(format(GrandLakesWatercorr/Squarecorr,'.2f'),'метров')
+world_water = 35_000_000                                               #Воды пресной всего в км^3
+Grand_Lakes_Water = World_Water * 1_000_000_000                        #Всего пресной воды в мире в м^3
+Grand_Lakes_Water_corr = Grand_Lakes_Water * 0.21                      #Всего пресной воды в Великих озерах в м^3
+Square = 7_664_000                                                     #Площадь 48 штатов США в км^2
+Square_corr = Square * 1_000_000                                       #Площадь 48 штатов США в м^2
+print(format(Grand_Lakes_Water_corr / Square_corr,'.2f'),'метров')

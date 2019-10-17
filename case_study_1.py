0f05cc169e6cd92e7a371b0156880892feefdd60
matthpatth <thealexzaytsev@gmail.com>
2019-10-17 23:16:33 +0700

    boat

--- case_study_1.py
+++ case_study_1.py
@@ -4,24 +4,71 @@
 #            Kurasova P.
 import turtle
 #TODO ALEXANDER
-turtle.penup()
-turtle.goto(-100,0)
-turtle.pencolor('yellow')
-turtle.begin_fill()
-turtle.color('blue')
+#turtle.penup()
+#turtle.goto(0,0)
+#turtle.pendown()
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(50)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(50)
+#turtle.end_fill()
+#turtle.up()
+#turtle.mainloop()
+
+turtle.goto(0,0)
 turtle.pendown()
-turtle.forward(100)
-turtle.left(90)
+turtle.forward(300)
+turtle.right(135)
+turtle.forward(105)
+turtle.right(45)
+turtle.forward(150)
+turtle.right(45)
+turtle.forward(105)
+turtle.up()
+
+turtle.right(135)
 turtle.forward(50)
+turtle.pendown()
 turtle.left(90)
 turtle.forward(100)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(100)
+turtle.up()
+
 turtle.left(90)
+turtle.forward(60)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(70)
+turtle.right(135)
 turtle.forward(50)
-turtle.end_fill()
+turtle.right(90)
+turtle.forward(50)
+turtle.right(135)
+turtle.up()
+turtle.forward(70)
+turtle.pendown()
+turtle.forward(50)
+turtle.right(135)
+turtle.forward(35)
+turtle.right(90)
+turtle.forward(35)
+turtle.right(135)
 turtle.up()
 turtle.mainloop()
 
-turtle
+
+
+
+
+
+
+
 #TODO ROMAN
 #TODO POLINA
 print('VSEM hi')

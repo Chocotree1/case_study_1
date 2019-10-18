15287cbc5276615afd16c106dea94e4a4003f6b7
Roman <sharypovr@mail.ru>
2019-10-19 03:36:25 +0700

    Complete

--- project1.py
+++ project1.py
@@ -1,6 +1,6 @@
-world_water = 35_000_000                                               #Воды пресной всего в км^3
-grand_lakes_water = world_water * 1_000_000_000                        #Всего пресной воды в мире в м^3
-grand_lakes_water_corr = grand_lakes_water * 0.21                      #Всего пресной воды в Великих озерах в м^3
-square = 7_664_000                                                     #Площадь 48 штатов США в км^2
-square_corr = square * 1_000_000                                       #Площадь 48 штатов США в м^2
-print(format(grand_lakes_water_corr / square_corr,'.2f'),'метров')
+world_water = 35_000_000                                               #Fresh water in km^3
+grand_lakes_water = world_water * 1_000_000_000                        #Fresh water in the world in m^3
+grand_lakes_water_corr = grand_lakes_water * 0.21                      #ВFresh water in Grand Lakes in m^3
+square = 7_664_000                                                     #Square of 48 states of USA in km^2
+square_corr = square * 1_000_000                                       #Square of 48 states of USA in m^2
+print(format(grand_lakes_water_corr / square_corr,'.2f'),'meters')

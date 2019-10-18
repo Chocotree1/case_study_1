e8635a51ede0249e28a9e9d445ee5fb067230b21
matthpatth <thealexzaytsev@gmail.com>
2019-10-18 09:15:58 +0700

    boat final

--- case_study_1.py
+++ case_study_1.py
@@ -17,7 +17,7 @@ import turtle
 #turtle.end_fill()
 #turtle.up()
 #turtle.mainloop()
-
+# trapetsiya
 turtle.goto(0,0)
 turtle.pendown()
 turtle.forward(300)
@@ -28,7 +28,7 @@ turtle.forward(150)
 turtle.right(45)
 turtle.forward(105)
 turtle.up()
-
+# pryamougolnik
 turtle.right(135)
 turtle.forward(50)
 turtle.pendown()
@@ -39,7 +39,7 @@ turtle.forward(40)
 turtle.right(90)
 turtle.forward(100)
 turtle.up()
-
+#treugolnik1
 turtle.left(90)
 turtle.forward(60)
 turtle.pendown()
@@ -51,6 +51,7 @@ turtle.right(90)
 turtle.forward(50)
 turtle.right(135)
 turtle.up()
+#treugolnik2
 turtle.forward(70)
 turtle.pendown()
 turtle.forward(50)
@@ -60,6 +61,37 @@ turtle.right(90)
 turtle.forward(35)
 turtle.right(135)
 turtle.up()
+#kvadrat
+turtle.right(180)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(70)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(30)
+turtle.up()
+
+#circles
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(120)
+turtle.pendown()
+turtle.circle(10)
+turtle.up()
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(5)
+turtle.left(90)
+turtle.forward(10)
+turtle.pendown()
+turtle.circle(10)
+
 turtle.mainloop()
 
 

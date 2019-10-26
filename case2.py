58969fba2e3934637b8eeace7b16c4e30cd301fb
matthpatth <thealexzaytsev@gmail.com>
2019-10-26 15:30:11 +0700

    input month's income

--- case2.py
+++ case2.py
@@ -2,7 +2,17 @@
 #            Sharypov R.
 #            Kurasova P.
 
+import local as lc
+
 #TODO ALEXANDER
+# string constants
+annual_income = 0
+for i in range(12):
+    print('Напишите Ваш ежемесячный доход за',i+1,'месяц года')
+    income = float(input())
+    annual_income = annual_income + income
+print(annual_income)
+
 #TODO ROMAN
 #TODO POLINA
 

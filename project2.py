dc5e34b57bb6459e18808b548b8611f19486af5e
Roman <sharypovr@mail.ru>
2019-10-26 14:45:21 +0700

    project2

--- project2.py
+++ project2.py
@@ -1,1 +1,12 @@
+a = float(input('First side: '))
+b = float(input('Second side: '))
+c = float(input('Third side: '))
 
+if (a + b <= c) or (a + c <= b) or (b + c <= a):
+    print('Triangle does not exist')
+elif (a != b) and (a != c) and (b != c):
+    print('versatile triangle')
+elif a == b == c:
+    print('equilateral triangle')
+else:
+    print('isosceles triangle')
